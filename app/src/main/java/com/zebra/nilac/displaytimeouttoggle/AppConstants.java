2313594258271894660982b13a0ab94f86d13d19
Calin Neamtu <nilac8991@gmail.com>
2022-07-18 09:41:48 +0200

    Code cleanup
    Signed-off-by: Calin Neamtu <nilac8991@gmail.com>

--- app/src/main/java/com/zebra/nilac/displaytimeouttoggle/AppConstants.java
+++ app/src/main/java/com/zebra/nilac/displaytimeouttoggle/AppConstants.java
@@ -3,23 +3,5 @@ package com.zebra.nilac.displaytimeouttoggle;
 public class AppConstants {
 
     public static final String PROFILE_NAME = "DisplayStayAwakeToggle";
-    public static final String ENABLE_STAY_AWAKE_PROFILE =
-            "<wap-provisioningdoc>\n" +
-                    "  <characteristic type=\"Profile\">\n" +
-                    "    <parm name=\"ProfileName\" value=\"DisplayStayAwakeToggle\"/>\n" +
-                    "    <characteristic version=\"4.3\" type=\"DisplayMgr\">\n"
-                    + "      <parm name=\"StayAwake\" value=\"1\" />\n"
-                    + "    </characteristic>\n" +
-                    "  </characteristic>" +
-                    "</wap-provisioningdoc>";
 
-    public static final String DISABLE_STAY_AWAKE_PROFILE =
-            "<wap-provisioningdoc>\n" +
-                    "  <characteristic type=\"Profile\">\n" +
-                    "    <parm name=\"ProfileName\" value=\"DisplayStayAwakeToggle\"/>\n" +
-                    "    <characteristic version=\"4.3\" type=\"DisplayMgr\">\n"
-                    + "      <parm name=\"StayAwake\" value=\"2\" />\n"
-                    + "    </characteristic>\n" +
-                    "  </characteristic>" +
-                    "</wap-provisioningdoc>";
 }
